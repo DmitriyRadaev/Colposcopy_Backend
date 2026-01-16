7bb3a24e8c041fe211b395861b5cb976b0b40b36
DmitriyRadaev <142414012+DmitriyRadaev@users.noreply.github.com>
2026-01-16 14:20:39 +0400

    Автоинкрементирование number в патологии

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 # models.py
 from django.conf import settings
-from django.db import models
+from django.db import models, transaction
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
@@ -148,7 +148,17 @@ class WorkerProfile(models.Model):
 class Pathology(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    number = models.IntegerField(null=True, blank=True)
+    number = models.IntegerField(null=True, blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if self.number is None:
+            with transaction.atomic():
+                # Блокируем таблицу для предотвращения конфликтов
+                max_num = Pathology.objects.select_for_update().aggregate(
+                    max_num=models.Max('number')
+                )['max_num']
+                self.number = (max_num or 0) + 1
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
